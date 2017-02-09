237c0df0da9507d6115b92bad2545add6fb3a037
karmami <karmami674+github@gmail.com>
2017-02-09 21:35:45 +0900

    Fix script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,16 @@
 #! /bin/sh
 if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-  echo "Start Deployment"
+  echo "install jsdoc"
   # Install modules
   npm i -g jsdoc &&
   # Publish to GitHub Pages
   ## Prepare ssh keys
+  echo 'set-up-ssh' &&
   $(npm bin)/set-up-ssh --key "$encrypted_8167579b67a3_key" \
                         --iv "$encrypted_8167579b67a3_iv" \
                         --path-encrypted-key ".travis/github_deploy_key.enc" &&
+  echo 'update-branch' &&
   $(npm bin)/update-branch --commands "$(dirname $0)/prepare-gh-pages.sh" \
                            --commit-message "Update website (${TRAVIS_TAG})" \
                            --directory "./public" \
--- scripts/prepare-gh-pages.sh
+++ scripts/prepare-gh-pages.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
 mkdir -p public &&
+rm -rf ./public/docs &&
+mkdir -p public/js &&
 ## Create a javascript file for web browsers
 gulp browserify &&
 ## Create JSDoc
@@ -10,9 +12,7 @@ git fetch &&
 git branch gh-pages origin/gh-pages &&
 git checkout gh-pages &&
 ## Add files to gh-pages
-rm -rf ./public/docs &&
 cp sample ./public/sample -r &&
 cp index.html ./public/ &&
-mkdir -p public/js &&
 cp ./vizdip.js ./public/js &&
-cp ./out ./public/docs -rx
+cp ./out ./public/docs -r
